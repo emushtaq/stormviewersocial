191044a1db1833d46d3b418d53bdb5f49a5f9dd1
emushtaq <eshanmushtaq@gmail.com>
2016-08-04 11:13:43 +0200

    Initial Commit from Storm Viewer

---
+++ Storm Viewer/DetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Eshan on 7/29/16.
+//  Copyright © 2016 Eshan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = detailItem!
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+
+}
+
